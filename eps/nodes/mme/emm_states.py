d08f1b2d07c594c0ad322b20a979891b0aa88284
sbugur <serkanbugur@gmail.com>
2013-12-31 17:10:21 +0200

    EMM State Machine in MME

---
+++ eps/nodes/mme/emm_states.py
@@ -0,0 +1,58 @@
+'''
+Created on 16 Ara 2013
+
+@author: Serkan
+'''
+from eps.utils.io import IoService
+from eps.utils.statemachine import StateMachine
+from eps.utils.config import Configuration
+from eps.nodes.mme.states import Default
+from eps.utils.statemachine import State
+from eps.procedures.mme.s1ap import S1SetupProcedureHandler
+from ctypes.wintypes import BOOL
+
+Attach = EPS_bearer_context = DETACH = network = UeInit = impDETACH = TAU = LLF = None
+
+
+class EMMState(State):
+
+    def __init__(self, context):
+        super(EMMState, self).__init__()
+        self.ioService = context["ioService"]
+        self.config = context["config"]
+
+class Deregistered(EMMState):
+
+    def __init__(self, context):
+        super(Deregistered, self).__init__(context)
+    
+    Attach = True                                       #Attach successful and default EPS bearer context is activated
+    EPS_bearer_context = True
+    
+    def register(self):
+        self.changeState(Registered)
+    
+    
+        
+class Registered(EMMState):
+
+    def __init__(self, context):
+        super(Registered, self).__init__(context)
+    
+    network =True 
+    DETACH = True    
+    UeInit = impDETACH = True
+    TAU = False
+    def register(self):
+        self.changeState(Deregistered_Initiated)
+        self.changeState(Deregistered)
+        
+class Deregistered_Initiated(EMMState):
+    def __init__(self, context):
+        super(Registered, self).__init__(context)
+        
+    DETACH = LLF = True 
+    def register(self):
+        self.changeState(Deregistered)   
+        
+        
